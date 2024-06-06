1e644068ac20cc7b8cf9c3bb85f6aecc2bebe770
Kevin Neilson <kevin@papermoon.io>
2024-06-06 16:17:21 -0700

    next

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -50,6 +50,10 @@ const config: HardhatUserConfig = {
       url: `https://eth-mainnet.alchemyapi.io/v2/${providerApiKey}`,
       accounts: [deployerPrivateKey],
     },
+    moonbase: {
+      url: `https://moonbase-alpha.public.blastapi.io`,
+      accounts: [deployerPrivateKey],
+    },
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`,
       accounts: [deployerPrivateKey],
